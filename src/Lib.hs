72cb6382939cd900bab116bec497038ec7ffcc34
Sebastian Karlsson <sebastiankarlsson29@gmail.com>
2022-06-27 17:14:49 +0100

    First version. Does not work...

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
+    ( takeFor
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Timeout
+import System.CPUTime
+
+type Seconds = Integer
+
+takeFor :: Seconds -> [a] -> IO [a]
+takeFor n = takeFor' $ n * 10^12
+
+takeFor' :: Integer -> [a] -> IO [a]
+takeFor' _ [] = return []
+takeFor' n (x:xs) = do
+    time1 <- getCPUTime
+    y <- timeout (fromIntegral $ n*10^6) $ x `seq` return x
+    time2 <- getCPUTime
+    let time = time2 - time1
+    case y of
+        Nothing -> return []
+        Just x' -> fmap (x':) $ takeFor' (n-time) xs
